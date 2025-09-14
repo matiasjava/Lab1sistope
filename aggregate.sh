d6b60da7d7a7de8e252d7b65232b774d96ea39fd
matiasjava <matias.vejar@usach.cl>
2025-09-14 00:40:11 -0300

    Se agrega script 5: aggregate.sh

---
+++ aggregate.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+#Entrada: pid uid comm pcpu pmem timestamp
+#Salida: comm  cant  prom_cpu  max_cpu  prom_mem  max_mem
+#Descripción: Agrupa por "comm", calcula cantidad, promedios y máximos de CPU/MEM.
+aggregate() {
+  awk '
+    BEGIN {
+      FS = "[ \t]+"; # Separador
+      print "comm\tcant\tprom_cpu\tmax_cpu\tprom_mem\tmax_mem"; # Nombre de columnas
+    }
+  {
+    c = $3 # se toma el tercer arumento para agrupar por comm
+    cpu = $4 + 0 # cpu , el +0 es para forzar a numero
+    mem = $5 + 0 # memoria
+    n[c] = n[c] + 1 # cantidad de procesos
+    sum_cpu[c] = sum_cpu[c] + cpu # suma de cpu
+    sum_mem[c] = sum_mem[c] + mem # suma de memoria
+    if (cpu > max_cpu[c] || n[c] == 1) max_cpu[c] = cpu  # revisa si cpu es mayor al maximo actual o si es el primer proceso
+    if (mem > max_mem[c] || n[c] == 1) max_mem[c] = mem  # revisa si mem es mayor al maximo actual o si es el primer proceso
+  }
+  END {
+    for (x in n) {
+      prom_cpu = sum_cpu[x] / n[x] # calcula promedio cpu
+      prom_mem = sum_mem[x] / n[x] # calcula promedio memoria
+      printf "%s\t%d\t%.2f\t%.2f\t%.2f\t%.2f\n", x, n[x], prom_cpu, max_cpu[x], prom_mem, max_mem[x]
+    }
+  }
+'
+}
+
+aggregate
