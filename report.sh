0af665f0489b921932cddb32f7e41a051c8cfd1e
matiasjava <matias.vejar@usach.cl>
2025-09-24 15:27:55 -0300

    Documentacion y pequeños cambios wuajajaj

--- report.sh
+++ report.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Definir flag -o y argumento
+# Definir flag -o utilizando getopts
 while getopts "o:" opt; do
   case $opt in o) outfile="$OPTARG"  ;; *)
       echo "Tienes que usar -o: $0 -o archivo.csv" >&2
@@ -20,31 +20,36 @@ fecha=$(date -Iseconds) # -Iseconds para obtener segundos en el formato ISO 8601
 usuario=$(whoami)
 host=$(hostname)
 
-# Escribir metadatos al principio
-{
-  echo "# generated_at: $fecha"
-  echo "# user: $usuario"
-  echo "# host: $host"
-  # Leer encabezado y poner comas para armar el csv
-  read -r encabezado
-  IFS=$'\t' read -ra cols <<< "$encabezado"
-  for i in "${!cols[@]}"; do
-    if [[ $i -eq 0 ]]; then
-      printf '"%s"' "${cols[$i]}"
-    else
-      printf ',"%s"' "${cols[$i]}"
-    fi
-  done
-  printf '\n'
-  # leer los datos (comm, cant, prom_cpu, max_cpu, prom_mem, max_mem) y poner comas
-  while IFS=$'\t' read -r -a fila; do
-    for j in "${!fila[@]}"; do
-      if [[ $j -eq 0 ]]; then
-        printf '"%s"' "${fila[$j]}"
+# Entradas: recibe por stdin los datos (comm  cant  prom_cpu  max_cpu  prom_mem  max_mem)
+# Salidas: escribe el reporte csv con metadatos en el archivo indicado por -o
+# Descripción: agrega metadatos (# fecha, # usuario, # host) y convierte a csv
+report() {
+  {
+    echo "# generated_at: $fecha"
+    echo "# user: $usuario"
+    echo "# host: $host"
+    # Leer nombres del encabezado y poner comas para armar el csv
+    read -r encabezado
+    IFS=$'\t' read -ra col <<< "$encabezado"
+    for i in "${!col[@]}"; do
+      if [[ $i -eq 0 ]]; then
+        printf '"%s"' "${col[$i]}"
       else
-        printf ',"%s"' "${fila[$j]}"
+        printf ',"%s"' "${col[$i]}"
       fi
     done
     printf '\n'
-  done
-} > "$outfile"
+    # leer los datos (comm, cant, prom_cpu, max_cpu, prom_mem, max_mem) y poner comas
+    while IFS=$'\t' read -r -a fila; do
+      for j in "${!fila[@]}"; do
+        if [[ $j -eq 0 ]]; then
+          printf '"%s"' "${fila[$j]}"
+        else
+          printf ',"%s"' "${fila[$j]}"
+        fi
+      done
+      printf '\n'
+    done
+  } > "$outfile"
+}
+report
