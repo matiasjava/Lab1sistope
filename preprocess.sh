f6d62c2119af69d8a619d87d5000717738549ac3
cristopher-torres <cristopher.torres@usach.cl>
2025-09-25 00:43:27 -0300

    Ultima documentacion jeje

--- preprocess.sh
+++ preprocess.sh
@@ -8,26 +8,26 @@ preprocess() {
   case "$1" in 
     --iso8601) usar_iso=true; shift ;; 
     "" ) ;; 
-    * ) echo "flag no reconocida: $1 , usa --iso8601 como primer argumento" >&2; exit 1 ;;
+    * ) echo "flag no reconocida: $1 , usa --iso8601 como primer argumento" >&2; exit 1 ;; # validacion de flag correcta
   esac
 
-  # Leer stdin línea por línea
+  # leer linea por linea de stdin
   while read -r line; do
     read -r timestamp pid uid comm pcpu pmem <<< "$line"
 
     # Validaciones
-    [[ "$timestamp" =~ ^[0-9]+$ ]] || continue
-    [[ "$pid" =~ ^[0-9]+$ ]] || continue
-    [[ "$uid" =~ ^[0-9]+$ ]] || continue
-    [[ "$pcpu" =~ ^[0-9]+([.][0-9]+)?$ ]] || continue
-    [[ "$pmem" =~ ^[0-9]+([.][0-9]+)?$ ]] || continue
+    [[ "$timestamp" =~ ^[0-9]+$ ]] || continue  # timestamp entero       
+    [[ "$pid" =~ ^[0-9]+$ ]] || continue  # pid entero
+    [[ "$uid" =~ ^[0-9]+$ ]] || continue  # uid entero
+    [[ "$pcpu" =~ ^[0-9]+([.][0-9]+)?$ ]] || continue  # pcpu decimal
+    [[ "$pmem" =~ ^[0-9]+([.][0-9]+)?$ ]] || continue  # pmem decimal
 
-    # Comprobar si se usó --iso8601
+    # Comprobar si se uso --iso8601
     if $usar_iso; then
       iso=$(date -d @"$timestamp" +"%Y-%m-%dT%H:%M:%S%z") || continue
-      printf "%s\t%s\t%s\t%s\t%s\t%s\n" "$iso" "$pid" "$uid" "$comm" "$pcpu" "$pmem"
+      printf "%s\t%s\t%s\t%s\t%s\t%s\n" "$iso" "$pid" "$uid" "$comm" "$pcpu" "$pmem"  # se escribe la linea con timestamp en ISO8601
     else
-      printf "%s\t%s\t%s\t%s\t%s\t%s\n" "$timestamp" "$pid" "$uid" "$comm" "$pcpu" "$pmem"
+      printf "%s\t%s\t%s\t%s\t%s\t%s\n" "$timestamp" "$pid" "$uid" "$comm" "$pcpu" "$pmem" # se escribe la linea original
     fi
   done
 }
